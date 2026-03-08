45a5e27f21add91dc1f32d3e7453284e0d1a12a0
asadktp <asadmolvi86@gmail.com>
2026-03-08 21:54:53 +0530

    fix: upgrade compileSdk to 36 required by plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.digitaldaftar.digital_daftar_network"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
